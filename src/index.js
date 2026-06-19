0dcb876f4ccc22a71eccc91e6124468fbb06ea67
otmarycarolina-ship-it <otmarycarolina@gmail.com>
2026-06-19 11:47:12 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-Import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { createRoot } from 'react-dom/client';
 import { initializeApp } from 'firebase/app';
 import { 
